f4e7de564102f6d4588835ef25f146332b7fd6b6
middlefgr <2giwon@gmail.com>
2020-08-02 23:32:31 +0900

    #07 메인 액티비티 패키지 이동

---
+++ app/src/main/java/com/egiwon/swiperecyclerviewsample/ui/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.egiwon.swiperecyclerviewsample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.egiwon.swiperecyclerviewsample.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main)
