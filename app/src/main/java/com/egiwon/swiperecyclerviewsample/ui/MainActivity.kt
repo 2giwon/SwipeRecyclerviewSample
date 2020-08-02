c69aefc6a2ba4881a86d9b89892d5618a74c1765
middlefgr <2giwon@gmail.com>
2020-08-03 00:14:42 +0900

    #15 어댑터 추가 처리 및 리소스 추가

--- app/src/main/java/com/egiwon/swiperecyclerviewsample/ui/MainActivity.kt
+++ app/src/main/java/com/egiwon/swiperecyclerviewsample/ui/MainActivity.kt
@@ -1,8 +1,48 @@
 package com.egiwon.swiperecyclerviewsample.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.egiwon.swiperecyclerviewsample.BR
 import com.egiwon.swiperecyclerviewsample.R
+import com.egiwon.swiperecyclerviewsample.base.BaseActivity
+import com.egiwon.swiperecyclerviewsample.base.BaseAdapter2
+import com.egiwon.swiperecyclerviewsample.databinding.ActivityMainBinding
+import com.egiwon.swiperecyclerviewsample.ui.model.SampleItem
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(R.layout.activity_main) {
+
+    override val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        bind {
+            vm = viewModel
+            val adapter = object : BaseAdapter2(
+                BR.item,
+                mapOf(BR.vm to viewModel),
+                mapOf(SampleItem::class to R.layout.item_sample)
+            ) {}
+
+            rvSample.adapter = adapter
+            val itemTouchHelper = ItemTouchHelper(
+                object : SwipeHelper(this@MainActivity, rvSample) {
+                    override fun instantiateUnderlayButton(
+                        viewHolder: RecyclerView.ViewHolder?,
+                        underlayButtons: List<UnderButton>?
+                    ) {
+
+                    }
+
+                })
+
+            itemTouchHelper.attachToRecyclerView(rvSample)
+
+
+        }
+    }
+}
