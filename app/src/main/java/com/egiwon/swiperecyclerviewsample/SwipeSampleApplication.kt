0823a8cd7a7622982028dc48f8c386a109cd73f0
middlefgr <2giwon@gmail.com>
2020-08-02 23:32:54 +0900

    #098 application 추가

---
+++ app/src/main/java/com/egiwon/swiperecyclerviewsample/SwipeSampleApplication.kt
@@ -0,0 +1,7 @@
+package com.egiwon.swiperecyclerviewsample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SwipeSampleApplication : Application()
